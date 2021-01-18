a09924956382c88865d95aa12b117b9165730a24
Vladimir Stoilov <Vladimir.Stoilov@bosch.com>
2021-01-18 12:34:30 +0200

    Basic Raspberry pi 3B support

--- build.zig
+++ build.zig
@@ -8,13 +8,17 @@ const panic = std.debug.panic;
 const stdout = std.io.getStdOut().outStream();
 
 pub fn build(b: *Builder) !void {
-    const teensy = b.option(bool, "teensy", "Build for teensy 3.2") orelse true;
+    const teensy = b.option(bool, "teensy", "Build for teensy 3.2") orelse false;
+    const raspberry = b.option(bool, "raspberry", "Build for raspberry pi 3b") orelse true;
 
     const firmware = b.addExecutable("firmware", "src/zrt.zig");
     firmware.addBuildOption(bool, "teensy3_2", teensy);
+    firmware.addBuildOption(bool, "raspberry", raspberry);
 
     if (teensy) {
         try teensyBuild(b, firmware);
+    } else if (raspberry) {
+        try raspberryBuild(b, firmware);
     } else {
         panic("Target not supported", .{});
     }
@@ -70,6 +74,41 @@ fn teensyBuild(b: *Builder, firmware: *LibExeObjStep) !void {
     upload.dependOn(&teensy_upload.step);
 
     b.default_step.dependOn(hex);
+}
+
+fn raspberryBuild(b: *Builder, firmware: *LibExeObjStep) !void {
+    try stdout.print("Building for Raspberry pi 3B\n", .{});
+
+    const target = CrossTarget{
+        .cpu_arch = .aarch64,
+        .os_tag = .freestanding,
+        .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a53 },
+    };
+    firmware.setTarget(target);
+    firmware.setLinkerScriptPath("src/raspberry/link/cortex-a.ld");
+    firmware.setOutputDir("zig-cache");
+    firmware.setBuildMode(builtin.Mode.Debug);
+
+    const dis = b.step("dis", "Disassemble");
+    const qemu = b.step("qemu", "Run the firmware in qemu");
+    const qemu_debug = b.step("qemu_debug", "Run the firmware in qemu");
+
+    var qemu_args = generateQemuArguments(b, "aarch64.exe", "raspi3", firmware.getOutputPath());
+    const run_qemu = b.addSystemCommand(qemu_args.items);
+    run_qemu.step.dependOn(&firmware.step);
+    qemu.dependOn(&run_qemu.step);
+    
+    var qemu_debug_args = generateQemuDebugArguments(b, "aarch64.exe", "raspi3", firmware.getOutputPath());
+    const run_qemu_debug = b.addSystemCommand(qemu_debug_args.items);
+    run_qemu_debug.step.dependOn(&firmware.step);
+    qemu_debug.dependOn(&run_qemu_debug.step);
+
+    var disassemble_args = generateDissasembleArguments(b, firmware.getOutputPath());
+    const run_disassemble = b.addSystemCommand(disassemble_args.items);
+    run_disassemble.step.dependOn(&firmware.step);
+    dis.dependOn(&run_disassemble.step);
+
+    b.default_step.dependOn(&firmware.step);
 }
 
 fn generateHexArguments(b: *Builder, firmwarePath: []const u8) std.ArrayList([]const u8) {
--- src/zrt.zig
+++ src/zrt.zig
@@ -1,11 +1,14 @@
 const build_options = @import("build_options");
 const teensy3_2 = build_options.teensy3_2;
+const raspberry = build_options.raspberry;
 
 const Driver = @import("driver.zig");
 
 const time = {
     if (teensy3_2) {
         return @import("teensy3_2/time.zig");
+    } else if (raspberry) {
+        return @import("raspberry/time.zig");
     } else {
         return {};
     }
@@ -14,6 +17,8 @@ const time = {
 const uart = {
     if (teensy3_2) {
         return @import("teensy3_2/uart.zig");
+    } else if (raspberry) {
+        return @import("raspberry/uart.zig");
     } else {
         return {};
     }
@@ -22,31 +27,18 @@ const uart = {
 pub const gpio = {
     if (teensy3_2) {
         return @import("teensy3_2/gpio.zig");
+    } else if (raspberry) {
+        return @import("raspberry/gpio.zig");
     } else {
         return {};
     }
 };
-
-pub const systick = {
-    if (teensy3_2) {
-        return @import("teensy3_2/systick.zig");
-    } else {
-        return {};
-    }
-};
-
-const start = {
-    if (teensy3_2) {
-        return @import("teensy3_2/startup.zig");
-    } else {
-        return {};
-    }
-};
-
 
 pub const init = {
     if (teensy3_2) {
         return @import("teensy3_2/init.zig");
+    } else if(raspberry) {
+        return @import("raspberry/init.zig");
     } else {
         return {};
     }
